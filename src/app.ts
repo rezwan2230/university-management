1582bf1a8f094d27a637bdf74a8a23df2e096fdb
rezwan2230 <rezwan2230@gmail.com>
2025-10-24 22:32:29 +0600

    refactoring routing

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,16 @@
-/* eslint-disable no-unused-vars */
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/students/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
-// import { StudentsRoutes } from './app/modules/students/student.route';
 //parsar
 app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use('/api/v1', router);
 
 //! Not Found routes
 app.use(notFound);
---
+++ src/app/routes/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { UserRoutes } from '../modules/user/user.route';
+import { StudentRoutes } from '../modules/students/student.route';
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
